178dc5970bc69fc7bb649dd2b14b71823a543667
Keviner <hsnks100@gmail.com>
2019-01-28 10:21:05 +0900

    scroll logic is changed

--- main.cpp
+++ main.cpp
@@ -5,65 +5,81 @@
 #include <string>
 #include <memory>
 #include <map>
+#include <cstring>
 
 
-
+void wCenterTitle(WINDOW *pwin, const char * title)
+{
+    int x, maxy, maxx, stringsize;
+    getmaxyx(pwin, maxy, maxx);
+    stringsize = 4 + strlen(title);
+    x = (maxx - stringsize)/2;
+    mvwaddch(pwin, 0, x, ACS_RTEE);
+    waddch(pwin, ' ');
+    waddstr(pwin, title);
+    waddch(pwin, ' ');
+    waddch(pwin, ACS_LTEE);
+}
 
 class LogWindow {
-    public:
-        virtual ~LogWindow() {
-            destroy_win();
-            printw("~LogWindow\n");
-            refresh();
-        }
-        std::deque<std::string> m_lines;
-        WINDOW* m_window;
-        int m_h, m_w, m_y, m_x;
-        void createWindow(int h, int w, int y, int x) {
-            WINDOW* win = newwin(h, w, y, x);
-            box(win, 0 , 0);          /* 0, 0 gives default characters
-                                             * for the vertical and horizontal
-                                             * lines                        */
-            wrefresh(win);            /* Show that box                */
-            m_h = h;
-            m_w = w;
-            m_y = y;
-            m_x = x;
-            wborder(win, '|', '|', '-', '-', '+', '+', '+', '+');
-            m_window = win;
-        }
-        void destroy_win()
-        {
-            wborder(m_window, ' ', ' ', ' ',' ',' ',' ',' ',' ');
-            /* The parameters taken are
-             * 1. win: the window on which to operate
-             * 2. ls: character to be used for the left side of the window
-             * 3. rs: character to be used for the right side of the window
-             * 4. ts: character to be used for the top side of the window
-             * 5. bs: character to be used for the bottom side of the window
-             * 6. tl: character to be used for the top left corner of the window
-             * 7. tr: character to be used for the top right corner of the window
-             * 8. bl: character to be used for the bottom left corner of the window
-             * 9. br: character to be used for the bottom right corner of the window
-             */
-            wrefresh(m_window);
-            delwin(m_window);
-        }
-        void print(const std::string& st) {
-            m_lines.push_back(st);
-            if(m_lines.size() >= m_h - 1) {
-                m_lines.pop_front();
-            }
-            for(int i=0; i<m_lines.size(); i++) {
-                mvwprintw(m_window, i + 1, 1, m_lines[i].c_str());
-            }
+public:
+    LogWindow(const std::string& t) {
+        m_title = t;
+    }
+    virtual ~LogWindow() {
+        destroy_win();
+        printw("~LogWindow\n");
+        refresh();
+    }
+    std::deque<std::string> m_lines;
+    std::string m_title = "TITLE";
+    WINDOW* m_window;
+    int m_h, m_w, m_y, m_x;
+    void createWindow(int h, int w, int y, int x) {
+        WINDOW* win = newwin(h, w, y, x);
 
-            wrefresh(m_window);
-        }
+        scrollok(win, 1);
+        box(win, 2 , 2);          /* 0, 0 gives default characters
+                                   * for the vertical and horizontal
+                                   * lines                        */
+        // wCenterTitle(win, m_title.c_str());
+        m_h = h;
+        m_w = w;
+        m_y = y;
+        m_x = x;
+        wborder(win, '|', '|', '-', '-', '+', '+', '+', '+');
+        m_window = win;
+    }
+    void refresh() {
+        wrefresh(m_window);
+    }
+    void destroy_win()
+    {
+        delwin(m_window);
+    }
+    void print(const std::string& st) {
+        // m_lines.push_back(st);
+        // if(m_lines.size() >= m_h - 1) {
+        //     m_lines.pop_front();
+        // }
+        // for(int i=0; i<m_lines.size(); i++) {
+        //     mvwprintw(m_window, i + 1, 1, m_lines[i].c_str());
+        // }
+
+        auto t = st + "\n";
+        waddch(m_window, ' ');
+        box(m_window, 0 , 0);
+        wprintw(m_window, t.c_str());
+        wborder(m_window, '|', '|', '-', '-', '+', '+', '+', '+');
+        // wCenterTitle(m_window, m_title.c_str());
+        wrefresh(m_window);
+    }
 };
 
 class LogNode;
+std::map<std::string, std::shared_ptr<LogWindow>> windows;
 using NodePointer = std::shared_ptr<LogNode>;
+using LogWindowHash = std::map<std::string, std::shared_ptr<LogWindow>>;
 struct LogNode : public std::enable_shared_from_this<LogNode> {
     std::shared_ptr<LogNode> m_left = nullptr;
     std::shared_ptr<LogNode> m_right = nullptr;
@@ -168,6 +184,51 @@ struct LogNode : public std::enable_shared_from_this<LogNode> {
         }
         return nullptr;
     }
+    LogWindowHash createWindows(int st, int h, int w, int y, int x) {
+        LogWindowHash lwh;
+        __drawWindows(shared_from_this(), st, h, w, y, x, lwh);
+        return lwh;
+    }
+    void __drawWindows(std::shared_ptr<LogNode> ln, int st, int h, int w, int y, int x, LogWindowHash& lwh) {
+        if(ln->m_left == nullptr && ln->m_right == nullptr) {
+            ln->m_window = std::shared_ptr<LogWindow>(new LogWindow(ln->m_name));
+            ln->m_window->createWindow(h, w, y, x);
+            lwh[ln->m_name] = ln->m_window;
+        }
+
+        // printw("%d %d\n", ln->, rightRatio);
+        // float leftRatio = ln->m_left->m_ratio / (float)(ln->m_left->m_ratio + ln->m_right->m_ratio);
+        // float rightRatio = ln->m_right->m_ratio / (float)(ln->m_left->m_ratio + ln->m_right->m_ratio);
+        // printf("%f %f\n", leftRatio, rightRatio);
+        // refresh();
+        // float leftRatio = ln->m_left->m_ratio / (float)(ln->m_left->m_ratio + ln->m_right->m_ratio);
+        float leftRatio = 1;
+        float rightRatio = 2;
+        // int rows, cols;
+        if(ln->m_left) {
+            leftRatio = ln->m_left->m_ratio / (float)(ln->m_left->m_ratio + ln->m_right->m_ratio);
+            rightRatio = ln->m_right->m_ratio / (float)(ln->m_left->m_ratio + ln->m_right->m_ratio);
+        }
+        if(ln->m_splitType == LogNode::SPLIT_TYPE::HORZ) {
+            if(ln->m_left) {
+                __drawWindows(ln->m_left, st + 1, h * leftRatio, w, y, x, lwh);
+            }
+            if(ln->m_right) {
+                __drawWindows(ln->m_right, st + 1, h * rightRatio, w, y + h * leftRatio, x, lwh);
+            }
+        }
+
+        if(ln->m_splitType == LogNode::SPLIT_TYPE::VERT) {
+            // printw("%f %f\n", leftRatio, rightRatio);
+            // refresh();
+            if(ln->m_left) {
+                __drawWindows(ln->m_left, st + 1, h, w * leftRatio, y, x, lwh);
+            }
+            if(ln->m_right) {
+                __drawWindows(ln->m_right, st + 1, h, w * rightRatio, y, x + w * leftRatio, lwh);
+            }
+        }
+    }
     // std::pair<NodePointer, NodePointer> splitVert(const std::string& name1, const std::string& name2);
 };
 
@@ -183,46 +244,6 @@ struct LogNode : public std::enable_shared_from_this<LogNode> {
 
 
 
-std::map<std::string, std::shared_ptr<LogWindow>> windows;
-void drawWindows(std::shared_ptr<LogNode> ln, int st, int h, int w, int y, int x) {
-    if(ln->m_left == nullptr && ln->m_right == nullptr) {
-        ln->m_window = std::shared_ptr<LogWindow>(new LogWindow());
-        ln->m_window->createWindow(h, w, y, x);
-        windows[ln->m_name] = ln->m_window;
-    }
-
-     // printw("%d %d\n", ln->, rightRatio);
-    // float leftRatio = ln->m_left->m_ratio / (float)(ln->m_left->m_ratio + ln->m_right->m_ratio);
-    // float rightRatio = ln->m_right->m_ratio / (float)(ln->m_left->m_ratio + ln->m_right->m_ratio);
-    // printf("%f %f\n", leftRatio, rightRatio);
-    // refresh();
-    // float leftRatio = ln->m_left->m_ratio / (float)(ln->m_left->m_ratio + ln->m_right->m_ratio);
-    float leftRatio = 1;
-    float rightRatio = 2;
-    if(ln->m_left) {
-        leftRatio = ln->m_left->m_ratio / (float)(ln->m_left->m_ratio + ln->m_right->m_ratio);
-        rightRatio = ln->m_right->m_ratio / (float)(ln->m_left->m_ratio + ln->m_right->m_ratio);
-    }
-    if(ln->m_splitType == LogNode::SPLIT_TYPE::HORZ) {
-        if(ln->m_left) {
-            drawWindows(ln->m_left, st + 1, h * leftRatio, w, y, x);
-        }
-        if(ln->m_right) {
-            drawWindows(ln->m_right, st + 1, h * rightRatio, w, y + h * leftRatio, x);
-        }
-    }
-
-    if(ln->m_splitType == LogNode::SPLIT_TYPE::VERT) {
-        // printw("%f %f\n", leftRatio, rightRatio);
-        // refresh();
-        if(ln->m_left) {
-            drawWindows(ln->m_left, st + 1, h, w * leftRatio, y, x);
-        }
-        if(ln->m_right) {
-            drawWindows(ln->m_right, st + 1, h, w * rightRatio, y, x + w * leftRatio);
-        }
-    }
-}
 void check(std::shared_ptr<LogNode> ln, int st, int h, int w, int y, int x) {
     for(int i=0; i<st; i++) {
         printw(".");
@@ -262,13 +283,15 @@ void check(std::shared_ptr<LogNode> ln, int st, int h, int w, int y, int x) {
 }
 
 
-
 int main()
 {
     initscr(); /* Start curses mode */
+    noecho();
+    // attron(A_BOLD);
+
     std::shared_ptr<LogNode> root = std::shared_ptr<LogNode>(new LogNode());
     root->m_name = "r1";
-    auto p = root->splitVert("r2", 1, 10);
+    auto p = root->splitVert("r2", 2, 10);
     p->m_right->splitHoriz("r3", 4, 5);
     /*
       ----------------|            ----------------|      ----------------|
@@ -278,15 +301,16 @@ int main()
       |               |            |       |       |      |       | r3:5  |
       |               |            |       |       |      |       |       |
       ----------------             ----------------|      ----------------|
-     */
+    */
     // check(root, 0, LINES / 2, COLS, 0, 0);
-    drawWindows(root, 0, LINES, COLS, 0, 0);
+    auto lwh = root->createWindows(0, LINES, COLS, 0, 0);
     int i=0;
     while(1) {
         i++;
-        windows["r1"]->print(std::string("i'am r1: ") + std::to_string(i));
-        windows["r2"]->print(std::string("i'am r2: ") + std::to_string(i));
-        windows["r3"]->print(std::string("i'am r3: ") + std::to_string(i));
+        lwh["r1"]->print(std::string("i'am r1: ") + std::to_string(i));
+        lwh["r2"]->print(std::string("i'am r2: ") + std::to_string(i));
+        lwh["r3"]->print(std::string("i'am r3: ") + std::to_string(i));
+        // getch();
         usleep(50000);
     }
     // getch();
